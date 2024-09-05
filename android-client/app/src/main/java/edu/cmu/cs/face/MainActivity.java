f5ad1920f68f3db0a4337a05b8b8b366f62c11c7
T0ny8576 <qifeid@andrew.cmu.edu>
2024-09-05 17:47:27 -0400

    Add android client NTP clock sync and profiling code

--- android-client/app/src/main/java/edu/cmu/cs/face/MainActivity.java
+++ android-client/app/src/main/java/edu/cmu/cs/face/MainActivity.java
@@ -7,19 +7,32 @@ import androidx.camera.core.ImageAnalysis;
 import androidx.camera.core.ImageProxy;
 import androidx.camera.view.PreviewView;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.net.NetworkRequest;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.speech.tts.TextToSpeech;
 import android.util.Log;
-import android.view.View;
 import android.widget.ImageView;
 
-import com.google.protobuf.Any;
 import com.google.protobuf.ByteString;
-import com.google.protobuf.InvalidProtocolBufferException;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Locale;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.ConcurrentLinkedDeque;
 import java.util.function.Consumer;
 
 import edu.cmu.cs.gabriel.camera.CameraCapture;
@@ -27,9 +40,11 @@ import edu.cmu.cs.gabriel.camera.ImageViewUpdater;
 import edu.cmu.cs.gabriel.camera.YuvToJPEGConverter;
 import edu.cmu.cs.gabriel.client.comm.ServerComm;
 import edu.cmu.cs.gabriel.client.results.ErrorType;
+import edu.cmu.cs.gabriel.client.results.SendSupplierResult;
 import edu.cmu.cs.gabriel.protocol.Protos.InputFrame;
 import edu.cmu.cs.gabriel.protocol.Protos.ResultWrapper;
 import edu.cmu.cs.gabriel.protocol.Protos.PayloadType;
+import edu.cmu.cs.sntp.SntpClient;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -47,7 +62,60 @@ public class MainActivity extends AppCompatActivity {
     private boolean useBackCamera = true;
     private HashMap<String, Long> faceRecognized = new HashMap<>();
     private static final long nameTTSCoolDownTime = 3 * 60 * 1000L;
-    private static final int confidenceThreshold = 70;
+    private static final int confidenceThreshold = 75;
+
+    // NTP Clock Sync
+    public SntpClient sntpClient;
+    private static final String NTP_SERVER = "AC-NTP0.NET.CMU.EDU";
+    private Timer ntpTimer;
+    private Network ntpNetwork;
+    private static final long NTP_POLLING_INTERVAL = 15000;  // Request NTP clock sync every 15 sec
+    private static final int NTP_BURST_POLLING_COUNT = 10;
+    private static final int NTP_POLLING_TIMEOUT = 100;
+    private static final long NTP_RTT_TOLERANCE = 10;
+    private long minNtpRtt = NTP_POLLING_INTERVAL;
+    private long lastNtpOffset = 0L;
+    public boolean ntpReceived = false;
+    public ConnectivityManager connectivityManager;
+
+    // Latency Profiling
+    private static final int PROFILING_COUNT = 500;
+    private int sentFrameCount = 0;
+    private int receivedFrameCount = 0;
+    private boolean doneProfiling = false;
+    private FileWriter logFileWriter;
+    private final SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss-z", Locale.US);
+    private final String LOGFILE = "Client-Timing-" + sdf.format(new Date()) + ".txt";
+    public final ConcurrentLinkedDeque<String> logList = new ConcurrentLinkedDeque<>();
+
+    private class NtpTimerTask extends TimerTask {
+        @Override
+        public void run() {
+            // NTP burst polling
+            for (int ntpi = 0; ntpi < NTP_BURST_POLLING_COUNT; ntpi++) {
+                if (!sntpClient.requestTime(NTP_SERVER, NTP_POLLING_TIMEOUT, ntpNetwork)) {
+                    // "labgw.elijah.cs.cmu.edu"
+                    // "ec2-54-197-201-248.compute-1.amazonaws.com"
+                    // "time.cloudflare.com"
+                    Log.w(TAG, "Failed to request time from NTP server: Timed out after " +
+                            NTP_POLLING_TIMEOUT + " milliseconds.");
+                } else {
+                    long rtt = sntpClient.getRoundTripTime();
+                    if (rtt < minNtpRtt + NTP_RTT_TOLERANCE) {
+                        lastNtpOffset = sntpClient.getNtpTime() - sntpClient.getNtpTimeReference();
+                        ntpReceived = true;
+                        Log.w(TAG, "Time update from NTP server using " +
+                                connectivityManager.getNetworkInfo(ntpNetwork).getTypeName() +
+                                ". RTT = " + sntpClient.getRoundTripTime() +
+                                ", Offset = " + sntpClient.getClockOffset());
+                        if (rtt < minNtpRtt) {
+                            minNtpRtt = rtt;
+                        }
+                    }
+                }
+            }
+        }
+    }
 
     private final Consumer<ResultWrapper> consumer = resultWrapper -> {
 //        try {
@@ -61,22 +129,27 @@ public class MainActivity extends AppCompatActivity {
 //            Log.e(TAG, "Protobuf parse error", e);
 //        }
 
-            if (resultWrapper.getResultsCount() == 0) {
-                Log.w(TAG, "Server returned empty results.");
-                return;
-            }
+        if (resultWrapper.getResultsCount() == 0) {
+            Log.w(TAG, "Server returned empty results.");
+            return;
+        }
 
-            // Load the user guidance (audio, image/video) from the result wrapper
-            for (ResultWrapper.Result result : resultWrapper.getResultsList()) {
-                if (result.getPayloadType() == PayloadType.IMAGE) {
-                    ByteString annotatedJpeg = result.getPayload();
-                    annotationViewUpdater.accept(annotatedJpeg);
-                } else if (result.getPayloadType() == PayloadType.TEXT) {
-                    ByteString faceResults = result.getPayload();
-                    String faceResultsString = faceResults.toStringUtf8();
-                    speakOutNames(faceResultsString);
-                }
+        receivedFrameCount++;
+        String frameRecvString = receivedFrameCount + "\tClient Recv\t" + getNetworkTimeString() + "\n";
+
+        // Load the user guidance (audio, image/video) from the result wrapper
+        for (ResultWrapper.Result result : resultWrapper.getResultsList()) {
+            if (result.getPayloadType() == PayloadType.IMAGE) {
+                ByteString annotatedJpeg = result.getPayload();
+                annotationViewUpdater.accept(annotatedJpeg);
+            } else if (result.getPayloadType() == PayloadType.TEXT) {
+                ByteString faceResults = result.getPayload();
+                String faceResultsString = faceResults.toStringUtf8();
+                speakOutNames(faceResultsString);
             }
+        }
+        frameRecvString = frameRecvString +  receivedFrameCount + "\tClient Done\t" + getNetworkTimeString() + "\n";
+        logList.add(frameRecvString);
     };
 
     private void speakOutNames(String faceResultsString) {
@@ -105,6 +178,28 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public String getNetworkTimeString() {
+        if (ntpReceived) {
+            long ntpNow = SystemClock.elapsedRealtime() + lastNtpOffset;
+            return String.valueOf(ntpNow);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            return String.valueOf(SystemClock.currentNetworkTimeClock().millis());
+        }
+        return System.currentTimeMillis() + " WARNING_MAY_NOT_USE_NETWORK_TIME_CLOCK";
+    }
+
+    private void writeLog() {
+        try {
+            for (String logString: logList) {
+                logFileWriter.write(logString);
+            }
+            logFileWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -114,6 +209,69 @@ public class MainActivity extends AppCompatActivity {
         annotationViewUpdater = new ImageViewUpdater(annotationView);
         PreviewView viewFinder = findViewById(R.id.viewFinder);
         ImageView camButton = findViewById(R.id.imgSwitchCam);
+
+        // Connect to network and run the app
+        NetworkRequest appNetworkRequest = new NetworkRequest.Builder()
+                .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+                .build();
+        connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        ConnectivityManager.NetworkCallback appNetworkCallback = new ConnectivityManager.NetworkCallback() {
+            @Override
+            public void onAvailable(@NonNull Network network) {
+                Log.w(TAG, "Requested app network type available, using " +
+                        connectivityManager.getNetworkInfo(network).getTypeName() + " " +
+                        connectivityManager.getNetworkInfo(network).getSubtypeName());
+                Log.w(TAG, "Network binding for app: " + connectivityManager.bindProcessToNetwork(network));
+                setupComm();
+            }
+        };
+        connectivityManager.requestNetwork(appNetworkRequest, appNetworkCallback);
+
+        File logFile = new File(getExternalFilesDir(null), LOGFILE);
+        logFile.delete();
+        logFile = new File(getExternalFilesDir(null), LOGFILE);
+        try {
+            logFileWriter = new FileWriter(logFile, true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        yuvToJPEGConverter = new YuvToJPEGConverter(this, 100);
+        camButton.setOnClickListener(v -> {
+            if (useBackCamera) {
+                cameraCapture = new CameraCapture(
+                        MainActivity.this, analyzer, WIDTH, HEIGHT, viewFinder,
+                        CameraSelector.DEFAULT_FRONT_CAMERA, false);
+                useBackCamera = false;
+            } else {
+                cameraCapture = new CameraCapture(
+                        MainActivity.this, analyzer, WIDTH, HEIGHT, viewFinder,
+                        CameraSelector.DEFAULT_BACK_CAMERA, false);
+                useBackCamera = true;
+            }
+        });
+        cameraCapture = new CameraCapture(this, analyzer, WIDTH, HEIGHT, viewFinder,
+                CameraSelector.DEFAULT_BACK_CAMERA, false);
+    }
+
+    void setupComm() {
+        NetworkRequest ntpNetworkRequest = new NetworkRequest.Builder()
+                .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+                .build();
+        ConnectivityManager.NetworkCallback ntpNetworkCallback = new ConnectivityManager.NetworkCallback() {
+            @Override
+            public void onAvailable(@NonNull Network network) {
+                Log.w(TAG, "Requested NTP network type available, using " +
+                        connectivityManager.getNetworkInfo(network).getTypeName() + " " +
+                        connectivityManager.getNetworkInfo(network).getSubtypeName());
+                // Time synchronization using SNTP
+                ntpNetwork = network;
+                sntpClient = new SntpClient();
+                ntpTimer = new Timer();
+                ntpTimer.scheduleAtFixedRate(new NtpTimerTask(), 0, NTP_POLLING_INTERVAL);
+            }
+        };
+        connectivityManager.requestNetwork(ntpNetworkRequest, ntpNetworkCallback);
 
         Consumer<ErrorType> onDisconnect = errorType -> {
             Log.e(TAG, "Disconnect Error: " + errorType.name());
@@ -136,25 +294,7 @@ public class MainActivity extends AppCompatActivity {
             serverComm.send(inputFrame, SOURCE, /* wait */ true);
             readyForServer = true;
         };
-        this.textToSpeech = new TextToSpeech(getApplicationContext(), onInitListener);
-        yuvToJPEGConverter = new YuvToJPEGConverter(this, 100);
-
-        camButton.setOnClickListener(v -> {
-            if (useBackCamera) {
-                cameraCapture = new CameraCapture(
-                        MainActivity.this, analyzer, WIDTH, HEIGHT, viewFinder,
-                        CameraSelector.DEFAULT_FRONT_CAMERA, false);
-                useBackCamera = false;
-            } else {
-                cameraCapture = new CameraCapture(
-                        MainActivity.this, analyzer, WIDTH, HEIGHT, viewFinder,
-                        CameraSelector.DEFAULT_BACK_CAMERA, false);
-                useBackCamera = true;
-            }
-        });
-
-        cameraCapture = new CameraCapture(this, analyzer, WIDTH, HEIGHT, viewFinder,
-                CameraSelector.DEFAULT_BACK_CAMERA, false);
+        textToSpeech = new TextToSpeech(getApplicationContext(), onInitListener);
     }
 
     // Based on
@@ -170,7 +310,18 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public void analyze(@NonNull ImageProxy image) {
             if (readyForServer) {
-                serverComm.sendSupplier(() -> {
+                String frameLogString = "\tClient Gen \t" + getNetworkTimeString() + "\n";
+                if (receivedFrameCount >= PROFILING_COUNT) {
+                    if (!doneProfiling) {
+                        Log.w("PROFILE1", "Done Profiling.\n");
+                        writeLog();
+                        doneProfiling = true;
+                    }
+                    image.close();
+                    return;
+                }
+
+                SendSupplierResult result = serverComm.sendSupplier(() -> {
                     ByteString jpegByteString = yuvToJPEGConverter.convert(image);
 
 //                ToServerExtras toServerExtras = ToServerExtras.newBuilder()
@@ -184,11 +335,29 @@ public class MainActivity extends AppCompatActivity {
 //                        .setExtras(pack(toServerExtras))
                             .build();
                 }, SOURCE, /* wait */ false);
+
+                if (result == SendSupplierResult.SUCCESS) {
+                    sentFrameCount++;
+                    String frameSentString = sentFrameCount + frameLogString + sentFrameCount + "\tClient Send\t" +
+                            getNetworkTimeString() + "\n";
+                    logList.add(frameSentString);
+                } else {
+                    logList.add("Failed to sendSupplier: frame " + (sentFrameCount + 1) + " at" + frameLogString);
+                }
             }
             // The image has either been sent or skipped. It is therefore safe to close the image.
             image.close();
         }
     };
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        minNtpRtt = NTP_POLLING_INTERVAL;  // Reset minNtpRtt
+        if (ntpTimer != null) {
+            ntpTimer.cancel();
+        }
+    }
 
     @Override
     protected void onDestroy() {
---
+++ android-client/app/src/main/java/edu/cmu/cs/sntp/SntpClient.java
@@ -0,0 +1,302 @@
+/*
+ * Copyright (C) 2008 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package edu.cmu.cs.sntp;
+
+import android.net.Network;
+import android.os.SystemClock;
+import android.util.Log;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.Objects;
+import java.util.Random;
+import java.util.function.Supplier;
+
+
+/**
+ * Simple, single-use SNTP client class for retrieving network time.
+ *
+ * Sample usage:
+ * <pre>SntpClient client = new SntpClient();
+ * if (client.requestTime("time.foo.com")) {
+ *     long now = client.getNtpTime() + SystemClock.elapsedRealtime() - client.getNtpTimeReference();
+ * }
+ * </pre>
+ *
+ * <p>This class is not thread-safe.
+ *
+ * @hide
+ */
+
+public class SntpClient {
+    private static final String TAG = "SntpClient";
+    private static final int REFERENCE_TIME_OFFSET = 16;
+    private static final int ORIGINATE_TIME_OFFSET = 24;
+    private static final int RECEIVE_TIME_OFFSET = 32;
+    private static final int TRANSMIT_TIME_OFFSET = 40;
+    private static final int NTP_PACKET_SIZE = 48;
+    public static final int STANDARD_NTP_PORT = 123;
+    private static final int NTP_MODE_CLIENT = 3;
+    private static final int NTP_MODE_SERVER = 4;
+    private static final int NTP_MODE_BROADCAST = 5;
+    private static final int NTP_VERSION = 3;
+    private static final int NTP_LEAP_NOSYNC = 3;
+    private static final int NTP_STRATUM_DEATH = 0;
+    private static final int NTP_STRATUM_MAX = 15;
+
+    // The source of the current system clock time, replaceable for testing.
+    private final Supplier<Instant> mSystemTimeSupplier;
+    private final Random mRandom;
+    // The last offset calculated from an NTP server response
+    private long mClockOffset;
+    // The last system time computed from an NTP server response
+    private long mNtpTime;
+    // The value of SystemClock.elapsedRealtime() corresponding to mNtpTime / mClockOffset
+    private long mNtpTimeReference;
+    // The round trip (network) time in milliseconds
+    private long mRoundTripTime;
+
+    private static class InvalidServerReplyException extends Exception {
+        public InvalidServerReplyException(String message) {
+            super(message);
+        }
+    }
+
+    public SntpClient() {
+        this(Instant::now, defaultRandom());
+    }
+
+    public SntpClient(Supplier<Instant> systemTimeSupplier, Random random) {
+        mSystemTimeSupplier = Objects.requireNonNull(systemTimeSupplier);
+        mRandom = Objects.requireNonNull(random);
+    }
+
+    /**
+     * Sends an SNTP request to the given host and processes the response.
+     *
+     * @param host host name of the server.
+     * @param timeout network timeout in milliseconds. the timeout doesn't include the DNS lookup
+     *                time, and it applies to each individual query to the resolved addresses of
+     *                the NTP server.
+     * @param network network over which to send the request.
+     * @return true if the transaction was successful.
+     */
+    public boolean requestTime(String host, int timeout, Network network) {
+        DatagramSocket socket = null;
+        try {
+            InetAddress[] addresses = network.getAllByName(host);
+            InetAddress address = addresses[0];
+            socket = new DatagramSocket();
+            network.bindSocket(socket);
+            socket.setSoTimeout(timeout);
+            byte[] buffer = new byte[NTP_PACKET_SIZE];
+            DatagramPacket request = new DatagramPacket(buffer, buffer.length, address, STANDARD_NTP_PORT);
+            // set mode = 3 (client) and version = 3
+            // mode is in low 3 bits of first byte
+            // version is in bits 3-5 of first byte
+            buffer[0] = NTP_MODE_CLIENT | (NTP_VERSION << 3);
+            // get current time and write it to the request packet
+            final Instant requestTime = mSystemTimeSupplier.get();
+            final Timestamp64 requestTimestamp = Timestamp64.fromInstant(requestTime);
+            final Timestamp64 randomizedRequestTimestamp =
+                    requestTimestamp.randomizeSubMillis(mRandom);
+            final long requestTicks = SystemClock.elapsedRealtime();
+            writeTimeStamp(buffer, TRANSMIT_TIME_OFFSET, randomizedRequestTimestamp);  // TODO: We don't even need this
+            socket.send(request);
+            // read the response
+            DatagramPacket response = new DatagramPacket(buffer, buffer.length);
+            socket.receive(response);
+            final long responseTicks = SystemClock.elapsedRealtime();
+            final Instant responseTime = requestTime.plusMillis(responseTicks - requestTicks);
+            final Timestamp64 responseTimestamp = Timestamp64.fromInstant(responseTime);
+            // extract the results
+            final byte leap = (byte) ((buffer[0] >> 6) & 0x3);
+            final byte mode = (byte) (buffer[0] & 0x7);
+            final int stratum = (int) (buffer[1] & 0xff);
+            final Timestamp64 referenceTimestamp = readTimeStamp(buffer, REFERENCE_TIME_OFFSET);
+            final Timestamp64 originateTimestamp = readTimeStamp(buffer, ORIGINATE_TIME_OFFSET);
+            final Timestamp64 receiveTimestamp = readTimeStamp(buffer, RECEIVE_TIME_OFFSET);
+            final Timestamp64 transmitTimestamp = readTimeStamp(buffer, TRANSMIT_TIME_OFFSET);
+            // Do validation according to RFC
+            checkValidServerReply(leap, mode, stratum, transmitTimestamp, referenceTimestamp,
+                    randomizedRequestTimestamp, originateTimestamp);
+            long totalTransactionDurationMillis = responseTicks - requestTicks;
+            long serverDurationMillis =
+                    Duration64.between(receiveTimestamp, transmitTimestamp).toDuration().toMillis();
+            long roundTripTimeMillis = totalTransactionDurationMillis - serverDurationMillis;
+            Duration clockOffsetDuration = calculateClockOffset(requestTimestamp,
+                    receiveTimestamp, transmitTimestamp, responseTimestamp);
+            long clockOffsetMillis = clockOffsetDuration.toMillis();
+            Log.d(TAG, "NTP server: " + address + ", RTT: " + roundTripTimeMillis + ", Clock Offset: " + clockOffsetMillis);
+            // save our results - use the times on this side of the network latency
+            // (response rather than request time)
+            mClockOffset = clockOffsetMillis;
+            mNtpTime = responseTime.plus(clockOffsetDuration).toEpochMilli();
+            mNtpTimeReference = responseTicks;
+            mRoundTripTime = roundTripTimeMillis;
+        } catch (UnknownHostException e) {
+            Log.w(TAG, "Unknown host: " + host);
+            return false;
+        } catch (Exception ex) {
+            Log.w(TAG, ex.fillInStackTrace());
+            return false;
+        } finally {
+            if (socket != null) {
+                socket.close();
+            }
+        }
+        return true;
+    }
+
+    /** Performs the NTP clock offset calculation. */
+    public static Duration calculateClockOffset(Timestamp64 clientRequestTimestamp,
+                                                Timestamp64 serverReceiveTimestamp,
+                                                Timestamp64 serverTransmitTimestamp,
+                                                Timestamp64 clientResponseTimestamp) {
+        // According to RFC4330:
+        // t is the system clock offset (the adjustment we are trying to find)
+        // t = ((T2 - T1) + (T3 - T4)) / 2
+        //
+        // Which is:
+        // t = (([server]receiveTimestamp - [client]requestTimestamp)
+        //       + ([server]transmitTimestamp - [client]responseTimestamp)) / 2
+        //
+        // See the NTP spec and tests: the numeric types used are deliberate:
+        // + Duration64.between() uses 64-bit arithmetic (32-bit for the seconds).
+        // + plus() / dividedBy() use Duration, which isn't the double precision floating point
+        //   used in NTPv4, but is good enough.
+        return Duration64.between(clientRequestTimestamp, serverReceiveTimestamp)
+                .plus(Duration64.between(clientResponseTimestamp, serverTransmitTimestamp))
+                .dividedBy(2);
+    }
+
+    /**
+     * Returns the offset calculated to apply to the client clock to arrive at {@link #getNtpTime()}
+     */
+    public long getClockOffset() {
+        return mClockOffset;
+    }
+
+    /**
+     * Returns the time computed from the NTP transaction.
+     *
+     * @return time value computed from NTP server response.
+     */
+    public long getNtpTime() {
+        return mNtpTime;
+    }
+
+    /**
+     * Returns the reference clock value (value of SystemClock.elapsedRealtime())
+     * corresponding to the NTP time.
+     *
+     * @return reference clock corresponding to the NTP time.
+     */
+    public long getNtpTimeReference() {
+        return mNtpTimeReference;
+    }
+
+    /**
+     * Returns the round trip time of the NTP transaction
+     *
+     * @return round trip time in milliseconds.
+     */
+    public long getRoundTripTime() {
+        return mRoundTripTime;
+    }
+
+    private static void checkValidServerReply(
+            byte leap, byte mode, int stratum, Timestamp64 transmitTimestamp,
+            Timestamp64 referenceTimestamp, Timestamp64 randomizedRequestTimestamp,
+            Timestamp64 originateTimestamp) throws InvalidServerReplyException {
+        if (leap == NTP_LEAP_NOSYNC) {
+            throw new InvalidServerReplyException("unsynchronized server");
+        }
+        if ((mode != NTP_MODE_SERVER) && (mode != NTP_MODE_BROADCAST)) {
+            throw new InvalidServerReplyException("untrusted mode: " + mode);
+        }
+        if ((stratum == NTP_STRATUM_DEATH) || (stratum > NTP_STRATUM_MAX)) {
+            throw new InvalidServerReplyException("untrusted stratum: " + stratum);
+        }
+        if (!randomizedRequestTimestamp.equals(originateTimestamp)) {
+            throw new InvalidServerReplyException(
+                    "originateTimestamp != randomizedRequestTimestamp");
+        }
+        if (transmitTimestamp.equals(Timestamp64.ZERO)) {
+            throw new InvalidServerReplyException("zero transmitTimestamp");
+        }
+        if (referenceTimestamp.equals(Timestamp64.ZERO)) {
+            throw new InvalidServerReplyException("zero referenceTimestamp");
+        }
+    }
+
+    /**
+     * Reads an unsigned 32 bit big endian number from the given offset in the buffer.
+     */
+    private long readUnsigned32(byte[] buffer, int offset) {
+        int i0 = buffer[offset++] & 0xFF;
+        int i1 = buffer[offset++] & 0xFF;
+        int i2 = buffer[offset++] & 0xFF;
+        int i3 = buffer[offset] & 0xFF;
+        int bits = (i0 << 24) | (i1 << 16) | (i2 << 8) | i3;
+        return bits & 0xFFFF_FFFFL;
+    }
+
+    /**
+     * Reads the NTP time stamp from the given offset in the buffer.
+     */
+    private Timestamp64 readTimeStamp(byte[] buffer, int offset) {
+        long seconds = readUnsigned32(buffer, offset);
+        int fractionBits = (int) readUnsigned32(buffer, offset + 4);
+        return Timestamp64.fromComponents(seconds, fractionBits);
+    }
+
+
+    /**
+     * Writes the NTP time stamp at the given offset in the buffer.
+     */
+    private void writeTimeStamp(byte[] buffer, int offset, Timestamp64 timestamp) {
+        long seconds = timestamp.getEraSeconds();
+        // write seconds in big endian format
+        buffer[offset++] = (byte) (seconds >>> 24);
+        buffer[offset++] = (byte) (seconds >>> 16);
+        buffer[offset++] = (byte) (seconds >>> 8);
+        buffer[offset++] = (byte) (seconds);
+        int fractionBits = timestamp.getFractionBits();
+        // write fraction in big endian format
+        buffer[offset++] = (byte) (fractionBits >>> 24);
+        buffer[offset++] = (byte) (fractionBits >>> 16);
+        buffer[offset++] = (byte) (fractionBits >>> 8);
+        buffer[offset] = (byte) (fractionBits);
+    }
+
+    private static Random defaultRandom() {
+        Random random;
+        try {
+            random = SecureRandom.getInstanceStrong();
+        } catch (NoSuchAlgorithmException e) {
+            // This should never happen.
+            random = new Random(System.currentTimeMillis());
+        }
+        return random;
+    }
+}
